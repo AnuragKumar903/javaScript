6579a6d6b7891e99d374977e716c2ae603083e5b
Anurag Kumar <abasu903@gmail.com>
2026-02-11 23:40:24 +0530

    check palindrome

--- javaScript/day02.js
+++ javaScript/day02.js
@@ -5,3 +5,19 @@ for(var i=name.length-1; i>=0; i--){
     newName += name[i];
 }
 console.log(newName);
+
+// que 02  check a palindrome
+function palindrome(a){
+    var i = 0;
+    var j = a.length - 1;
+    while(i < j){
+        if(a[i].toLowerCase() !== a[j].toLowerCase())   return false;
+        i++;
+        j--;
+    }
+    return true;
+}
+
+let str = "Madam";
+let isPlaindrome = palindrome(str);
+console.log(isPlaindrome);
